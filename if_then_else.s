69d11fe890117cf92f6323810d743f7feac2dcbc
bhargav1236 <56440259+bhargav1236@users.noreply.github.com>
2020-10-01 16:00:15 +0530

    Create if_then_else.s

---
+++ if_then_else.s
@@ -0,0 +1,18 @@
+    AREA     appcode, CODE, READONLY
+     EXPORT __main
+	 ENTRY 
+__main  FUNCTION		         
+        MOV R2,#0x100 ; 
+        MOV R7,#0x100 ; 
+        MOV R4,#0x05 ; 
+	CMP R2,R7 ; 
+	ITTE NE    ; 
+	SUBNE R2,R2,R7 ;
+	MOVNE R2,#0    ; 
+	ADDEQ R2,R2,R4 ; 
+	MOVEQ R4,#0   ; 
+		
+stop    B stop ; stop program
+		 
+     ENDFUNC
+     END
