40299cd27ac0a00bc095b48b981b7c77ded1cd40
bhargav1236 <56440259+bhargav1236@users.noreply.github.com>
2020-11-08 17:49:51 +0530

    Update first.s

--- Neural_Network_Implementation/Sigmoid_Graph/first.s
+++ Neural_Network_Implementation/Sigmoid_Graph/first.s
@@ -5,12 +5,12 @@
 	 IMPORT printMsg4p
      ENTRY 
 __main  FUNCTION	
-	   LDR		R7, =385 ;
-       VLDR.F32		S20, = -12 ;Provide x to calculate ln(1+x) 
+	   LDR		R7, =385 ; plotting 385 points starting from -12 with increment of 0.0625
+       VLDR.F32		S20, = -12 ;Provide x to calculate 1+e^-x
 TEST	   LDR		R3, = 10000 ; loop variable
 	   VMOV.F32		S1,S20 ;Copying x to S1
 	   VNEG.F32		S2,S20 ;Copying -x to S2
-	   VLDR.F32		S5,=2 ;The Value of ln(1+x) after each iterarion is stored in S5
+	   VLDR.F32		S5,=2 ;The Value of 1+e^-x after each iterarion is stored in S5
 	   VLDR.F32		S19, = 1;
 	   VLDR.F32		S18, = 1;
 	   VLDR.F32		S16, = 0.0625;
@@ -30,8 +30,8 @@ LOOP 	VDIV.F32		S4,S2,S18 ;
 	   CMP			R3,#0	;compare if maimum iteration is reached 
 	   BNE 			LOOP	;Goto Next iteration if maximum iteration is not reached
 SIGM	VDIV.F32		S5,S19,S5 ; Computing [1/(1+e^-x)]
-		VCVT.U32.F32	S5,S5,#32
-		VMOV			R0, D2[1]
+		VCVT.U32.F32	S5,S5,#32 ; Converting Floating point number to fixed point number
+		VMOV			R0, D2[1] ;storing s5 to R0 in the form of fixed point Number
 		BL			printMsg ;
 		VADD.F32	S20,S16
 		SUB			R7,#1
